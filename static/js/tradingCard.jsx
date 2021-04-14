efa72611fdd29a87b84af10a549a002b97e693dd
Monica <monica@hackbrightacademy.com>
2021-04-14 04:03:14 +0000

    worked on reactdom and rendering a React component for each data point

---
+++ static/js/tradingCard.jsx
@@ -0,0 +1,173 @@
+"use strict";
+// const tradingCardData = [
+//   {
+//     name: 'Balloonicorn',
+//     skill: 'video games',
+//     imgUrl: '/static/img/balloonicorn.jpg'
+//   }
+// ]
+
+// function TradingCard(props) {
+//   return (
+//     <div className="card">
+//       <h2>Name: {props.name}</h2>
+//       <img src={props.imgUrl} />
+//       <h2>Skill: {props.skill}</h2>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Balloonicorn"
+//       skill="video games"
+//       imgUrl="/static/img/balloonicorn.jpg"
+//     />
+//   ),
+//   document.querySelector('#balloonicorn')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Float"
+//       skill="baking pretzels"
+//       imgUrl="/static/img/float.jpg"
+//     />
+//   ),
+//   document.querySelector('#float')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Llambda"
+//       skill="knitting scarves"
+//       imgUrl="/static/img/llambda.jpg"
+//     />
+//   ),
+//   document.querySelector('#llambda')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Merge"
+//       skill="swimming quickly"
+//       imgUrl="/static/img/merge.jpg"
+//     />
+//   ),
+//   document.querySelector('#merge')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Seedpy"
+//       skill="spitting seeds"
+//       imgUrl="/static/img/seedpy.jpg"
+//     />
+//   ),
+//   document.querySelector('#seedpy')
+// );
+
+const tradingCardData = [
+  {
+    name: 'Balloonicorn',
+    skill: 'video games',
+    imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
+  }
+];
+
+// for (const card of tradingCardData) {
+//   console.log(card.name);
+//   console.log(card.skill);
+// }
+
+// function TradingCardContainer() {
+//   const tradingCards = [];
+//   for (const card of tradingCardData) {
+//     tradingCards.push(
+//       <TradingCard
+//       name={card.name}
+//       skill={card.skill}
+//       imgURL={card.imgURL}
+//     />
+//     );
+//   }
+
+//   return (
+//     <React.Fragment>
+//       {tradingCards}
+//     </React.Fragment>
+//   );
+// }
+
+function TradingCardContainer() {
+  const tradingCards = [];
+
+  for (const card of tradingCardData) {
+    tradingCards.push(
+      <TradingCard
+        name={card.name}
+        skill={card.skill}
+        imgUrl={card.imgUrl}
+      />
+    );
+  }
+
+  return (
+    <React.Fragment>
+      {tradingCards}
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(
+  <TradingCardContainer/>,
+  document.querySelector('#container')
+);
