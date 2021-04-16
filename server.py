0bd6684a0f78ac52211aba5d622e9b1990850d3d
Monica <monica@hackbrightacademy.com>
2021-04-16 03:24:44 +0000

    worked and completed further study

--- server.py
+++ server.py
@@ -16,6 +16,11 @@ def show_cards():
 
     return render_template('cards.html')
 
+@app.route('/about-the-author')
+def author_info():
+    """page about the author."""
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
